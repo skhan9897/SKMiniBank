99ad40f3161ce00475805ad113a2915518924573
Sajid Khan <msazidraza0786@gmail.com>
2026-07-01 00:44:26 +0530

    update DBConnection

--- src/main/java/com/bank/util/DBConnection.java
+++ src/main/java/com/bank/util/DBConnection.java
@@ -5,33 +5,24 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/skminibank";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1234";   // अपना MySQL Password लिखें
+    private static final String URL =
+        "jdbc:mysql://bgu4lq3qy1n3jxgyg6nv-mysql.services.clever-cloud.com:3306/bgu4lq3qy1n3jxgyg6nv?useSSL=true&requireSSL=true&serverTimezone=UTC";
+
+    private static final String USERNAME = "usxiqkn8avl74kyr";
+    private static final String PASSWORD = "Gvx0N5WR2lcNVhTLuSbY";
 
     private static Connection con;
 
     public static Connection getConnection() {
-
         try {
-
             if (con == null || con.isClosed()) {
-
                 Class.forName("com.mysql.cj.jdbc.Driver");
-
                 con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
                 System.out.println("Database Connected Successfully");
-
             }
-
         } catch (Exception e) {
-
             e.printStackTrace();
-
         }
-
         return con;
     }
-
 }
